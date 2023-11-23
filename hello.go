42f15706d2d0a4e667623966f0a0efa7738440eb
Adriel Vieira Santos <adriel.klt@gmail.com>
2023-11-22 22:29:51 -0300

    adiciona função para monitoramento de sites

--- hello.go
+++ hello.go
@@ -2,23 +2,32 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"time"
+)
+
+const (
+	monitoramentos = 3
+	delay          = 5 * time.Second
 )
 
 func main() {
 	exibirIntroducao()
-	exibirMenu()
-	comando := lerComando()
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo logs...")
-	case 0:
-		fmt.Println("Saindo do programa...")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço esse comando")
+	for {
+		exibirMenu()
+		comando := lerComando()
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs...")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço esse comando")
+		}
 	}
 }
 
@@ -40,4 +49,30 @@ func lerComando() int {
 	fmt.Scan(&comando)
 	fmt.Println("O comando escolhido foi", comando)
 	return comando
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	sites := []string{
+		"https://alura.com.br",
+		"https://google.com.br",
+		"https://brisanet.com.br/teste",
+		"https://youtube.com.br/abc",
+	}
+	for ii := 0; ii < monitoramentos; ii++ {
+		for site := range sites {
+			fmt.Println("Testando site", site, ":", sites[site])
+			testarSite(sites[site])
+		}
+		time.Sleep(delay)
+	}
+}
+
+func testarSite(site string) {
+	resp, _ := http.Get(site)
+	if resp.StatusCode == http.StatusOK {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
 }
